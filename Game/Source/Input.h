46f8ef33b38f5542842836497f62575b08266909
Makinilla-maker <60987205+Makinilla-maker@users.noreply.github.com>
2021-04-17 19:57:43 +0200

    Gamepad
    Co-Authored-By: IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
    Co-Authored-By: isaac553876299 <60979696+isaac553876299@users.noreply.github.com>
    Co-Authored-By: paumotta <61042875+paumotta@users.noreply.github.com>

--- Game/Source/Input.h
+++ Game/Source/Input.h
@@ -2,10 +2,16 @@
 #define __INPUT_H__
 
 #include "Module.h"
+#include "SDL/include/SDL_gamecontroller.h"
+#include "SDL/include/SDL_haptic.h"
 
 //#define NUM_KEYS 352
 #define NUM_MOUSE_BUTTONS 5
 //#define LAST_KEYS_PRESSED_BUFFER 50
+
+
+#define MAX_KEYS 256
+#define MAX_PADS 4
 
 struct SDL_Rect;
 
@@ -23,6 +29,27 @@ enum KeyState
 	KEY_DOWN,
 	KEY_REPEAT,
 	KEY_UP
+};
+
+struct GamePad
+{
+public:
+	//Input data
+	bool start, back, guide;
+	bool x, y, a, b, l1, r1, l3, r3;
+	bool up, down, left, right;
+	float l2, r2;
+	float l_x, l_y, r_x, r_y, l_dz, r_dz;
+
+	//Controller data
+	bool enabled;
+	int index;
+	SDL_GameController* controller;
+	SDL_Haptic* haptic;
+
+	//Rumble controller
+	int rumble_countdown;
+	float rumble_strength;
 };
 
 class Input : public Module
@@ -65,6 +92,18 @@ public:
 	void GetMousePosition(int &x, int &y);
 	void GetMouseMotion(int& x, int& y);
 
+	void HandleDeviceConnection(int index);
+
+	// Deactivates SDL device funcionallity when a gamepad has been disconnected
+	void HandleDeviceRemoval(int index);
+
+	// Called at PreUpdate
+	// Iterates through all active gamepads and update all input data
+	void UpdateGamepadsInput();
+
+	bool ShakeController(int id, int duration, float strength = 0.5f);
+	const char* GetControllerName(int id) const;
+
 private:
 	bool windowEvents[WE_COUNT];
 	KeyState*	keyboard;
@@ -73,6 +112,14 @@ private:
 	int mouseMotionY;
 	int mouseX;
 	int mouseY;
+
+	// An array to fill in the state of all the keyboard keys
+
+public:
+
+	KeyState keys[MAX_KEYS] = { KEY_IDLE };
+	GamePad pads[MAX_PADS];
+
 };
 
 #endif // __INPUT_H__
